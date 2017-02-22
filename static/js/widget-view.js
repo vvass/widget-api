88a1f5b72e897b74a1620e7e8f2737334941cf32
vvass <vass_v@ymail.com>
2017-02-22 16:31:44 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -9,11 +9,21 @@ $(function(){
       type: 'GET',
       success: function(response){
         console.log(response);
+
+        $('#widget-grid').accordion();
+
+
+
+
       },
       error: function(error){
         console.log(error);
       }
     });
+  }
+
+  function widgetAccordion() {
+
   }
 
 
