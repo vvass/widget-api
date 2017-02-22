bdf29ddd854494a6ee438324aec25d3e231650ad
vvass <vass_v@ymail.com>
2017-02-22 13:06:52 -0500

    added createOrder endpoint

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, json, request, send_from_directory
 import MySQLdb
 import _mysql
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 tasks = [
     {
