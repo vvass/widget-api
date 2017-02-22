3f8beb3961c07c37429cb805dd46fafffc746a1c
vvass <vass_v@ymail.com>
2017-02-22 15:51:48 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -3,9 +3,18 @@ $(function(){
   alert("yo");
 
   $('body').ready(function(){
-    alert("yo");
+    console.log("You have body ready");
 
-    
+    $.ajax({
+      url: '/getAvalQuantity/6',
+      type: 'GET',
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.log(error);
+      }
+    });
 
 
   });
