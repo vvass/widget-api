7bb46fa3172c91273c768d1d0d851650c2273dbe
vvass <vass_v@ymail.com>
2017-02-22 15:46:20 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -1,20 +1,13 @@
 $(function(){
 
   alert("yo");
-  $('window').on(function(){
 
-    console.log("this is up");
+  $('body').ready(function(){
+    alert("yo");
 
-    // $.ajax({
-    //   url: '/widgetCreate',
-    //   data: $('form').serialize(),
-    //   type: 'POST',
-    //   success: function(response){
-    //     console.log(response);
-    //   },
-    //   error: function(error){
-    //     console.log(error);
-    //   }
-    // });
+    
+
+
   });
+
 });
