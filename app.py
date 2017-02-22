f5e187caa1a60291d43adc2b4745486ffc552643
vvass <vass_v@ymail.com>
2017-02-22 15:21:18 -0500

    added createOrder endpoint

--- app.py
+++ app.py
@@ -7,25 +7,6 @@ from widgets import test
 
 app = Flask(__name__, static_url_path='')
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
 # TEMAPLATE ROUTES
 
 @app.route('/enter', methods=['GET'])
@@ -208,7 +189,7 @@ def search_by_finish(fin):
         cursor.execute(query,param)
         data=cursor.fetchall()
 
-        return json.dumps({'success':str(test())})
+        return json.dumps({'success':str(data)})
 
 
     except Exception as e:
