3ad9b68e3e165e02ab9de2f1de7436c77338e718
vvass <vass_v@ymail.com>
2017-02-24 16:09:07 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -101,8 +101,8 @@ $(function(){
   function clickToEditInventory(item) {
     $(".ui-icon-pencil").click(function () {
       console.log(this);
-      $(this).empty();
-      $(this).html('Inventory: <input type="text"></p>');
+      // $(this).empty();
+      // $(this).html('Inventory: <input type="text"></p>');
       // $(this).html('<p>Inventory: ' + item[2] + ' <span class="ui-icon ui-icon-pencil"></span><p>');
 
     });
