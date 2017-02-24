0f9e37dfbfd11a0ae76b4b93bc6d8202a8fc840b
vvass <vass_v@ymail.com>
2017-02-24 09:53:41 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -142,11 +142,11 @@ $(function(){
       $("#checkbox-widget-options").append(
         '<fieldset>' +
           '<legend>Select a Size: </legend>' +
-            '<label for="radio-1">mapping[0][1]</label>' +
+            '<label for="radio-1">'+ mapping[0][1] +'</label>' +
             '<input type="radio" name="radio-1" id="radio-1">' +
-            '<label for="radio-2">mapping[1][1]</label>' +
+            '<label for="radio-2">'+ mapping[1][1] +'</label>' +
             '<input type="radio" name="radio-1" id="radio-2">' +
-            '<label for="radio-3">mapping[2][1]</label>' +
+            '<label for="radio-3">'+ mapping[2][1] +'</label>' +
             '<input type="radio" name="radio-1" id="radio-3">' +
         '</fieldset>');
 
