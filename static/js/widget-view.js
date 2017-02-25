80b1ca42fa66af06783bc9419b38dc53ab18988d
vvass <vass_v@ymail.com>
2017-02-24 21:45:27 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -110,7 +110,7 @@ $(function(){
         $(".editable-inventory-"+item[0])
           .append('<span id="temporary-input-span-' + item[0] + '">Inventory: <input type="text"></span>');
 
-        $("#temporary-input-span")
+        $("#temporary-input-span"+item[0])
           .focusout(function () {
             console.log("focusout",item);
           });
