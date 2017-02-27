c06d31f050bed6ac5d94d3b96ca7859129715329
vvass <vass_v@ymail.com>
2017-02-27 11:40:40 -0500

    added createOrder endpoint

--- static/js/orders-view.js
+++ static/js/orders-view.js
@@ -70,7 +70,7 @@ $(function(){
       url: '/getWidgetName/' + id,
       type: 'GET',
       success: function(response){
-        console.log(mapResults(response)[0]);
+        console.log(mapResults(response)[0][0]);
       },
       error: function(error){
         console.log(error);
