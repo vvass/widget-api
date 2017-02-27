d9e790a9202727d440fb8ca5d243ed7aa8d36980
vvass <vass_v@ymail.com>
2017-02-27 14:48:39 -0500

    added createOrder endpoint

--- static/js/widget-view.js
+++ static/js/widget-view.js
@@ -105,8 +105,7 @@ $(function(){
     $("#widget-grid")
       .find("h3")
       .each(function () {
-        $(this).after('   <span class="ui-icon ui-icon-plus"></span>')
-
+        $(this).append('   <span class="ui-icon ui-icon-plus"></span>');
       });
 
   }
