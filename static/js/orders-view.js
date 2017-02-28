63e6ad839a51a51b2a0f4eb5b4535ee4dfc2cedd
vvass <vass_v@ymail.com>
2017-02-28 00:23:21 -0500

    added createOrder endpoint

--- static/js/orders-view.js
+++ static/js/orders-view.js
@@ -43,7 +43,6 @@ $(function(){
         if (!e.model.isNew()) {
           // Disable the editor of the "id" column when editing data items
           var numeric = e.container.find("input[name=amount]").data("kendoNumericTextBox");
-          numeric.enable(false);
         }
       },
       columns: [{
