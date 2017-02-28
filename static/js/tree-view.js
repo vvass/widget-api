4d02c59e068625325bf3275deca0425f45057b2d
vvass <vass_v@ymail.com>
2017-02-28 14:33:15 -0500

    added createOrder endpoint

--- static/js/tree-view.js
+++ static/js/tree-view.js
@@ -1,175 +1,151 @@
 $(document).ready(function () {
-  var tempSelectList = [];
   var dialog = $("#dialog").kendoDialog({
-    width: "400px",
-    height: "500px",
+    width: "800px",
+    height: "520px",
     visible: false,
-    title: "Employees",
+    title: "Members",
     closable: true,
     modal: false,
-    content: "<div class='k-textbox k-space-right search-wrapper'><input id='employees-search' type='text'  placeholder='Search employees'/><span class='k-icon k-i-search'></span></div>" +
-    "<div class='select-all-wrapper'><input data-role='checkbox' onchange='selectAll(this)' type='checkbox' class='k-checkbox' id='_selectAllEmployees'/><label class='k-checkbox-label' for='_selectAllEmployees'>Select all employees</label><span class='selected-count'></span></div>" +
-    "<div id='treeview'></div>",
+    content: "<div id='treelist'></div>",
     actions: [
-      { text: 'Cancel'},
+      { text: 'Cancel' },
       { text: 'OK', primary: true, action: actionOK }
     ],
-    initOpen: initOpen,
-    open: dialogOpen
+    initOpen: initOpen
   });
 
   $("#pickEmployeesButton").kendoButton({
     click: openDialog
   });
 
-  var serviceRoot = "https://demos.telerik.com/kendo-ui/service";
-  homogeneous = new kendo.data.HierarchicalDataSource({
+  var crudServiceBaseUrl = "https://demos.telerik.com/kendo-ui/service";
+
+  var dataSource = new kendo.data.TreeListDataSource({
     transport: {
       read: {
-        url: serviceRoot + "/Employees",
+        url: crudServiceBaseUrl + "/EmployeeDirectory/All",
         dataType: "jsonp"
+      },
+      update: {
+        url: crudServiceBaseUrl + "/EmployeeDirectory/Update",
+        dataType: "jsonp"
+      },
+      destroy: {
+        url: crudServiceBaseUrl + "/EmployeeDirectory/Destroy",
+        dataType: "jsonp"
+      },
+      create: {
+        url: crudServiceBaseUrl + "/EmployeeDirectory/Create",
+        dataType: "jsonp"
+      },
+      parameterMap: function (options, operation) {
+        if (operation !== "read" && options.models) {
+          return { models: kendo.stringify(options.models) };
+        }
       }
     },
+    batch: true,
     schema: {
       model: {
         id: "EmployeeId",
-        hasChildren: "HasEmployees"
+        parentId: "ReportsTo",
+        fields: {
+          EmployeeId: { type: "number", editable: false, nullable: false },
+          ReportsTo: { nullable: true, type: "number" },
+          FirstName: { validation: { required: true } },
+          LastName: { validation: { required: true } },
+          HireDate: { type: "date" },
+          Phone: { type: "string" },
+          HireDate: { type: "date" },
+          BirthDate: { type: "date" },
+          Extension: { type: "number", validation: { min: 0, required: true } },
+          Position: { type: "string" }
+        },
+        expanded: true
       }
     }
   });
 
-  $("#treeview").kendoTreeView({
-    dataSource: homogeneous,
-    dataTextField: "FullName",
-    checkboxes: true,
-    loadOnDemand: false,
-    expandAll: true,
-    dataBound: treeViewDataBound,
-    check: treeViewCheck
+  $("#treelist").kendoTreeList({
+    dataSource: dataSource,
+    height: 380,
+    filterable: true,
+    sortable: true,
+    toolbar: ["create"],
+    columns: [
+      {
+        headerTemplate: "<input type='checkbox' onclick='toggleAll(event)' />",
+        template: "<input type='checkbox' class='checkbox' data-bind='checked: checked' />",
+        width: 40,
+        filterable: false
+      },
+      {
+        field: "FirstName", title: "First Name", width: 170,
+        expandable: true
+      },
+      { field: "LastName", title: "Last Name", width: 110 },
+      { field: "Position" },
+      {
+        title: "Edit", command: ["edit", "destroy"], width: 250,
+        attributes: {
+          style: "text-align: center;"
+        }
+      }
+    ],
+    dataBound: treeListDataBound
   });
 });
 
-function treeViewDataBound(e) {
-  e.sender.expand(e.node);
-}
-
-function initOpen(e) {
-  $("#employees-search").on("input", function () {
-    var query = this.value.toLowerCase();
-    var dataSource = $("#treeview").data("kendoTreeView").dataSource;
-    filter(dataSource, query);
-    matchColors(query);
+function treeListDataBound(e) {
+  $(".checkbox").bind("change", function (e) {
+    var row = $(e.target).closest("tr");
+    this.checked ? row.addClass("k-state-selected") : row.removeClass("k-state-selected");
   });
 }
 
-function dialogOpen(e) {
-  var treeView = $("#treeview").data("kendoTreeView");
-  tempSelectList = getCheckedItems(treeView);
+function toggleAll(e) {
+  if (e.target.checked) {
+    $("#treelist [role='row'] .checkbox").each(function () {
+      this.checked = "checked";
+    });
+  }
+  else {
+    $("#treelist [role='row'] .checkbox").removeAttr("checked");
+  }
+
+  $("#treelist [role='row'] .checkbox").trigger("change");
+}
+
+function initOpen(e) {
   setTimeout(function () {
-    $("#employees-search").focus().select();
+    $("#treelist").data("kendoTreeList").refresh();
   })
 }
+
 function openDialog(e) {
   $("#dialog").data("kendoDialog").open();
 }
 
 function actionOK(e) {
-  var treeView = $("#treeview").data("kendoTreeView");
-  var checkedNodes = getCheckedItems(treeView);
-  updateResult(checkedNodes);
+  var treelist = $("#treelist").data("kendoTreeList");
+  var items = treelist.element.find(".k-state-selected");
+  updateResult(items, treelist);
 }
 
-function updateResult(checkedNodes) {
-  if (checkedNodes.length > 0) {
+function updateResult(items, treelist) {
+  if (items.length > 0) {
     var result = "";
-    for (var i = 0; i < checkedNodes.length; i++) {
-      result += "<span class='selectedName'>" + checkedNodes[i].FullName + "</span>";
+    for (var i = 0; i < items.length; i++) {
+      var dataItem = treelist.dataItem(items[i]);
+      result += "<span class='selectedName'>" + dataItem.FirstName + " " + dataItem.LastName + "</span>";
     }
   } else {
-    result = "No employees selected.";
+    result = "No members selected.";
   }
 
   $("#result").html(result);
-}
-function treeViewCheck(e) {
-  setTimeout(function () {
-    updateSelectedCount(e.sender);
-  })
-}
-
-function selectAll(sender) {
-  $("#treeview .k-checkbox").removeAttr("checked").prop("checked", sender.checked);
-  $("#treeview .k-checkbox").trigger("change");
 }
 
 function updateSelectedCount(treeView) {
   $(".selected-count").html(getCheckedItems(treeView).length + " Employees selected");
 }
-
-function getCheckedItems(treeview) {
-  var nodes = treeview.dataSource.view();
-  return getCheckedNodes(nodes);
-}
-
-function getCheckedNodes(nodes) {
-  var node, childCheckedNodes;
-  var checkedNodes = [];
-
-  for (var i = 0; i < nodes.length; i++) {
-    node = nodes[i];
-    if (node.checked) {
-      checkedNodes.push(node);
-    }
-
-    if (node.hasChildren) {
-      childCheckedNodes = getCheckedNodes(node.children.view());
-      if (childCheckedNodes.length > 0) {
-        checkedNodes = checkedNodes.concat(childCheckedNodes);
-      }
-    }
-
-  }
-
-  return checkedNodes;
-}
-
-function filter(dataSource, query) {
-  var hasVisibleChildren = false;
-  var data = dataSource instanceof kendo.data.HierarchicalDataSource && dataSource.data();
-
-  for (var i = 0; i < data.length; i++) {
-    var item = data[i];
-    var text = item.FullName.toLowerCase();
-    var itemVisible =
-      query === true // parent already matches
-      || query === "" // query is empty
-      || text.indexOf(query) >= 0; // item text matches query
-
-    var anyVisibleChildren = filter(item.children, itemVisible || query); // pass true if parent matches
-
-    hasVisibleChildren = hasVisibleChildren || anyVisibleChildren || itemVisible;
-
-    item.hidden = !itemVisible && !anyVisibleChildren;
-  }
-
-  if (data) {
-    // re-apply filter on children
-    dataSource.filter({ field: "hidden", operator: "neq", value: true });
-  }
-
-  return hasVisibleChildren;
-}
-
-function matchColors(query, element) {
-  $("#treeview .k-in:containsIgnoreCase('" + query + "')").each(function () {
-    var index = $(this).html().toLowerCase().indexOf(query.toLowerCase());
-    var length = query.length;
-    var original = $(this).html().substr(index, length);
-    var newText = $(this).html().replace(original, "<span class='query-match'>" + original + "</span>");
-    $(this).html(newText);
-  });
-}
-
-$.expr[':'].containsIgnoreCase = function (n, i, m) {
-  return jQuery(n).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
-};
