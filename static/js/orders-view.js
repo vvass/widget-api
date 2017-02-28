682ea13f4d1950d82fc6c73783c043c95b7914d4
vvass <vass_v@ymail.com>
2017-02-27 23:56:12 -0500

    added createOrder endpoint

--- static/js/orders-view.js
+++ static/js/orders-view.js
@@ -124,6 +124,11 @@ $(function(){
               var grid = $("#grid").data("kendoGrid");
               grid.setDataSource(dataSource);
               console.log("here");
+              $("#grid > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(1) > td:nth-child(6) > a").kendoButton({
+                click: function() {
+                  console.log("somethign");
+                }
+              });
 
             }
 
@@ -153,6 +158,7 @@ $(function(){
 
     $("#grid > div.k-grid-content.k-auto-scrollable > table > tbody > tr:nth-child(1) > td:nth-child(6) > a").click(function () {
       console.log("and here");
+      alert("asdfasdf");
     });
 
 
