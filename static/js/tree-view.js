7e01a4a2c44088739cc2d728e474a4df7e6c3626
vvass <vass_v@ymail.com>
2017-02-28 16:09:10 -0500

    added createOrder endpoint

--- static/js/tree-view.js
+++ static/js/tree-view.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  var orderData = [];
+  var widgetData = [];
 
 
   data = [
@@ -55,59 +55,60 @@ $(document).ready(function () {
     $.ajax({
       url: '/getWidgets',
       type: 'GET',
+      success: function (result) {
+        var array = mapResults(result);
+        console.log(array);
+
+        for(var i = 0; i < array.length; i++) {
+
+          if(array[i][6] == 0)
+            array[i][6] = null;
+
+          widgetData.push({
+            "id": array[i][0],
+            "name": array[i][1],
+            "inventory": array[i][2],
+            "finish": array[i][3],
+            "size": array[i][4],
+            "types": array[i][5],
+            "parentId": array[i][6]
+          });
+        }
+
+        console.log(widgetData);
+
+        var dataSource = new kendo.data.TreeListDataSource({
+          data: widgetData,
+
+          schema: {
+            model: {
+              id: "id",
+              expanded: true
+            }
+          }
+        });
+
+        console.log(data);
+
+        $("#treelist").kendoTreeList({
+          dataSource: dataSource,
+          height: 540,
+          columns: [
+            { field: "id",title: "Id" },
+            { field: "name",title: "Name" },
+            { field: "inventory",title: "Inventory" },
+            { field: "finish",title: "Finish" },
+            { field: "size",title: "Size" },
+            { field: "types",title: "Types" },
+            { field: "parentId",title: "" }
+          ]
+        });
+
+      },
       error: function(error){
         console.log(error);
       }
     }).then(function (result) {
-      var array = mapResults(result);
-      console.log(array);
-
-      for(var i = 0; i < array.length; i++) {
-
-        if(array[i][6] == 0)
-          array[i][6] = null;
-
-        orderData.push({
-          "id": array[i][0],
-          "name": array[i][1],
-          "inventory": array[i][2],
-          "finish": array[i][3],
-          "size": array[i][4],
-          "types": array[i][5],
-          "parentId": array[i][6]
-        });
-      }
-
-      console.log(orderData);
-
-      orderData = [{}]
-
-      var dataSource = new kendo.data.TreeListDataSource({
-        data: data,
-
-        schema: {
-          model: {
-            id: "id",
-            expanded: true
-          }
-        }
-      });
-
-      console.log(data);
-
-      $("#treelist").kendoTreeList({
-        dataSource: dataSource,
-        height: 540,
-        columns: [
-          { field: "id",title: "Id" },
-          { field: "name",title: "Name" },
-          { field: "inventory",title: "Inventory" },
-          { field: "finish",title: "Finish" },
-          { field: "size",title: "Size" },
-          { field: "types",title: "Types" },
-          { field: "parentId",title: "" }
-        ]
-      });
 
     });
   }
