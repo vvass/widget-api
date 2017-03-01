2e76139783fe9d4328b3d8706188fb4eab1cd013
vvass <vass_v@ymail.com>
2017-03-01 11:09:30 -0500

    added createOrder endpoint

--- static/js/tree-view.js
+++ static/js/tree-view.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
                     { field: "id",title: "Id",hidden: true },
                     { field: "name",title: "Name" },
                     { field: "inventory",title: "Inventory"},
-                    { field: "finish",title: "Finish", editor: categoryDropDownEditor, template: "#=Category.CategoryName#" },
+                    { field: "finish",title: "Finish", editor: categoryDropDownEditor, template: "#=finish.CategoryName#" },
                     { field: "size",title: "Size" },
                     { field: "types",title: "Type" },
                     { field: "parentId",title: "",hidden: true },
