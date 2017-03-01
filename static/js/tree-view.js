c18fdfb3b82cab206872ba29c6465d4d38ceacce
vvass <vass_v@ymail.com>
2017-03-01 09:41:32 -0500

    added createOrder endpoint

--- static/js/tree-view.js
+++ static/js/tree-view.js
@@ -11,78 +11,69 @@ $(document).ready(function () {
       success: function (result) {
         var array = mapResults(result);
 
-        $.when(function () {
-          for(var i = 0; i < array.length; i++) {
+        for(var i = 0; i < array.length; i++) {
 
-            if(array[i][6] == 0)
-              array[i][6] = null;
+          if(array[i][6] == 0)
+            array[i][6] = null;
 
-            var getSize = fetchData('/getSizes', array[i][4]),
-              getFinish = fetchData('/getFinishes',array[i][3]),
-              getTypes = fetchData('/getTypes',array[i][5]);
+          var getSize = fetchData('/getSizes', array[i][4]),
+            getFinish = fetchData('/getFinishes',array[i][3]),
+            getTypes = fetchData('/getTypes',array[i][5]);
 
-            $.when(array, i, getSize, getFinish, getTypes)
-              .then(function (array, i, size, finish, types){
+          $.when(array, i, getSize, getFinish, getTypes)
+            .then(function (array, i, size, finish, types) {
 
-                widgetData.push({
-                  "id": array[i][0],
-                  "name": array[i][1],
-                  "inventory": array[i][2],
-                  "finish": JSON.parse(size[0]).success.replace(/\(+|,|\)|'/g,''),
-                  "size": JSON.parse(finish[0]).success.replace(/\(+|,|\)|'/g,''),
-                  "types": JSON.parse(types[0]).success.replace(/\(+|,|\)|'/g,''),
-                  "parentId": array[i][6]
-                });
-
+              widgetData.push({
+                "id": array[i][0],
+                "name": array[i][1],
+                "inventory": array[i][2],
+                "finish": JSON.parse(size[0]).success.replace(/\(+|,|\)|'/g,''),
+                "size": JSON.parse(finish[0]).success.replace(/\(+|,|\)|'/g,''),
+                "types": JSON.parse(types[0]).success.replace(/\(+|,|\)|'/g,''),
+                "parentId": array[i][6]
               });
-          }
 
-
-          console.log(widgetData);
-
-
-        }).then(function () {
-
-          console.log(widgetData);
-          var dataSource = new kendo.data.TreeListDataSource({
-            data: widgetData,
-            schema: {
-              model: {
-                id: "id",
-                expanded: true
+              if(orderData.length-1 == i) {
+                console.log("done", widgetData);
               }
-            },
-            error: function (e) {
-              console.log("Error");
-            },
-            change: function (e) {
-              console.log("Change");
-            },
-            requestStart: function (e) {
-              console.log("Request Start");
+
+            });
+        }
+
+        console.log(widgetData);
+
+        var dataSource = new kendo.data.TreeListDataSource({
+          data: widgetData,
+          schema: {
+            model: {
+              id: "id",
+              expanded: true
             }
-          });
-
-          $("#treelist").kendoTreeList({
-            dataSource: dataSource,
-            height: 540,
-            columns: [
-              { field: "id",title: "Id",hidden: false },
-              { field: "name",title: "Name" },
-              { field: "inventory",title: "Inventory" },
-              { field: "finish",title: "Finish" },
-              { field: "size",title: "Size" },
-              { field: "types",title: "Types" },
-              { field: "parentId",title: "",hidden: true }
-            ]
-          });
-
-
+          },
+          error: function (e) {
+            console.log("Error");
+          },
+          change: function (e) {
+            console.log("Change");
+          },
+          requestStart: function (e) {
+            console.log("Request Start");
+          }
         });
 
-
-
-
+        $("#treelist").kendoTreeList({
+          dataSource: dataSource,
+          height: 540,
+          columns: [
+            { field: "id",title: "Id",hidden: false },
+            { field: "name",title: "Name" },
+            { field: "inventory",title: "Inventory" },
+            { field: "finish",title: "Finish" },
+            { field: "size",title: "Size" },
+            { field: "types",title: "Types" },
+            { field: "parentId",title: "",hidden: true }
+          ]
+        });
 
 
 
